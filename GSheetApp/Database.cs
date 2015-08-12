64ac3ab5e6c0bacbd4382c96ec386451f4b0d59c
milezoom <mu51im.x28@gmail.com>
2015-08-12 14:42:11 +0700

    * Database.cs: * packages.config: * GSheetApp.csproj: * gui.stetic: * generated.cs: * MainWindow.cs: * SpreadsheetInteraction.cs:
    * app.config: add file Database.cs, SpreadsheetInteraction.cs to
      solution

---
+++ GSheetApp/Database.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace GSheetApp
+{
+	public class Database
+	{
+		private MySqlConnection connection;
+
+		public Database (string host, string dbName, string username, string password)
+		{
+			string connectionString = 
+				"SERVER=" + host + ";" +
+				"DATABASE=" + dbName + ";" +
+				"UID=" + username + ";" +
+				"PASSWORD=" + password + ";" +
+				"Allow Zero Datetime=TRUE" + ";";
+
+			this.connection = new MySqlConnection (connectionString);
+		}
+
+		public List<List<string>> runQuery (string query)
+		{
+			string cleanQuery = sanitizeQuery (query);
+			var result = new List<List<string>> ();
+			try {
+				this.connection.Open ();
+
+				MySqlCommand command = new MySqlCommand (cleanQuery, this.connection);
+				MySqlDataReader reader = command.ExecuteReader ();
+
+				while (reader.Read ()) {
+					List<string> data = new List<string> ();
+					for (int i = 0; i < reader.FieldCount; i++) {
+						data.Add (reader [i].ToString ());
+					}
+					result.Add (data);
+				}
+
+				this.connection.Close ();
+			} catch (Exception e) {
+				Console.WriteLine (e);
+			}
+			return result;
+		}
+
+		public string sanitizeQuery (string query)
+		{
+			string result = "";
+
+			// clean malicious command
+			var regexAlter = new Regex (";alter ", RegexOptions.IgnoreCase);
+			var regexUpdate = new Regex (";update ", RegexOptions.IgnoreCase);
+			var regexDelete = new Regex (";delete ", RegexOptions.IgnoreCase);
+			var regexInsert = new Regex (";insert ", RegexOptions.IgnoreCase);
+			var regexDrop = new Regex (";drop ", RegexOptions.IgnoreCase);
+
+			result = regexAlter.Replace (query, "");
+			result = regexDelete.Replace (result, "");
+			result = regexDrop.Replace (result, "");
+			result = regexInsert.Replace (result, "");
+			result = regexUpdate.Replace (result, "");
+			result = Regex.Replace (result, "([Aa][Ll][Tt][Ee][Rr])", "");
+			result = Regex.Replace (result, "([Dd][Ee][Ll][Ee][Tt][Ee])", "");
+			result = Regex.Replace (result, "([Dd][Rr][Oo][Pp])", "");
+			result = Regex.Replace (result, "([Ii][Nn][Ss][Ee][Rr][Tt])", "");
+			result = Regex.Replace (result, "([Uu][Pp][Dd][Aa][Tt][Ee])", "");
+
+			// clean malicious char
+			var regexComment = new Regex (";--");
+			result = regexComment.Replace (query, "");
+			result = Regex.Replace (result, "(--)", "");
+
+			// clean additional terminate char
+			result = Regex.Replace (result, ";", "");
+
+			// add terminate char to end of query
+			result = result + ";";
+
+			return result;
+		}
+	}
+}
---
+++ GSheetApp/SpreadsheetInteraction.cs
@@ -0,0 +1,150 @@
+﻿using System;﻿
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Auth.OAuth2.Flows;
+using Google.Apis.Drive.v2;
+using Google.Apis.Drive.v2.Data;
+using Google.Apis.Plus.v1;
+using Google.Apis.Util;
+using Google.Apis.Util.Store;
+using Google.Apis.Services;
+
+using Google.GData.Client;
+using Google.GData.Spreadsheets;
+
+namespace GSheetApp
+{
+	public class SpreadsheetInteraction
+	{
+		private string[] SCOPE = {
+			DriveService.Scope.Drive + " " +
+			"https://spreadsheets.google.com/feeds " +
+			PlusService.Scope.UserinfoEmail
+		};
+		private string APPLICATION_NAME = "GSheetApp";
+		private string REDIRECT_URI = "urn:ietf:wg:oauth:2.0:oob";
+		private UserCredential credential;
+		private ClientSecrets secret;
+
+		public SpreadsheetInteraction ()
+		{
+			Console.WriteLine ("Begin Create Spreadsheet Integration");
+			var stream = new FileStream ("client_secret.json", FileMode.Open, FileAccess.Read);
+			this.secret = GoogleClientSecrets.Load (stream).Secrets;
+			Console.WriteLine ("Secret Saved");
+
+			using (stream) {
+				string credPath = Directory.GetCurrentDirectory ();
+				credPath = Path.Combine (credPath, ".credentials");
+
+				this.credential = GoogleWebAuthorizationBroker.AuthorizeAsync (
+					secret, SCOPE, "user", CancellationToken.None, new FileDataStore (credPath, true)
+				).Result;
+			}
+
+			stream.Close ();
+		}
+
+		public DriveService createDriveService ()
+		{
+			return new DriveService (new BaseClientService.Initializer () {
+				HttpClientInitializer = credential,
+				ApplicationName = APPLICATION_NAME,
+			});
+		}
+
+		public PlusService createPlusService ()
+		{
+			return new PlusService (new BaseClientService.Initializer () {
+				HttpClientInitializer = credential,
+				ApplicationName = APPLICATION_NAME,
+			});
+		}
+
+		public string createSheet (DriveService service, string filename)
+		{
+			var file = new Google.Apis.Drive.v2.Data.File ();
+			file.Title = filename;
+			file.Description = string.Format ("Created via {0} at {1}", APPLICATION_NAME, DateTime.Now.ToString ());
+			file.MimeType = "application/vnd.google-apps.spreadsheet";
+
+			var request = service.Files.Insert (file);
+			var result = request.Execute ();
+			return result.Id;
+		}
+
+		public SpreadsheetsService createSheetService ()
+		{
+			OAuth2Parameters parameters = new OAuth2Parameters ();
+			parameters.ClientId = this.secret.ClientId;
+			parameters.ClientSecret = this.secret.ClientSecret;
+			parameters.RedirectUri = REDIRECT_URI;
+			parameters.Scope = SCOPE.ToString ();
+			parameters.AccessToken = credential.Token.AccessToken;
+
+			var spreadsheetsService = new SpreadsheetsService (APPLICATION_NAME);
+			spreadsheetsService.RequestFactory = new GOAuth2RequestFactory (null, "GSheetApp-v1", parameters);
+			return spreadsheetsService;
+		}
+
+		public SpreadsheetEntry getSheetEntry (SpreadsheetsService service, string fileId)
+		{
+			var query = new SpreadsheetQuery (
+				            "https://spreadsheets.google.com/feeds/spreadsheets/" + fileId
+			            );
+			var feed = service.Query (query);
+			return (SpreadsheetEntry)feed.Entries [0];
+		}
+
+		public WorksheetEntry getWorksheetEntry (SpreadsheetEntry entry, int wsId)
+		{
+			WorksheetFeed wsFeed = entry.Worksheets;
+			return (WorksheetEntry)wsFeed.Entries [wsId];
+		}
+
+		public CellFeed getCellFeed (WorksheetEntry entry, SpreadsheetsService service)
+		{
+			CellQuery cellQuery = new CellQuery (entry.CellFeedLink);
+			return service.Query (cellQuery);
+		}
+
+		public int[] writeToSheet (List<List<string>> data, string filename)
+		{
+			int[] result = { 0, 0 };
+			string sheetId = createSheet (createDriveService (), filename);
+			Console.WriteLine ("success creating new sheet");
+			SpreadsheetEntry sheetEntry = getSheetEntry (createSheetService (), sheetId);
+			Console.WriteLine ("success get sheet entry");
+			WorksheetEntry wsEntry = getWorksheetEntry (sheetEntry, 0);
+			wsEntry.Rows = Convert.ToUInt32 (data.Count);
+			wsEntry.Cols = Convert.ToUInt32 (data [0].Count);
+			wsEntry.Update ();
+			Console.WriteLine ("success get worksheet entry");
+			CellFeed feed = getCellFeed (wsEntry, createSheetService ());
+			Console.WriteLine ("success get cell feed");
+			CellEntry entry;
+
+			try {
+				for (uint i = 0; i < data.Count; i++) {
+					List<string> row = data [Convert.ToInt32 (i)];
+					for (uint j = 0; j < row.Count; j++) {
+						entry = new CellEntry (i + 1, j + 1, (row [Convert.ToInt32 (j)]).ToString ());
+						Console.WriteLine ("insert data into cel " + (i + 1) + (j + 1));
+						feed.Insert (entry);
+						Console.WriteLine ("success inserting data");
+					}
+				}
+				Console.WriteLine ("success writing data to new sheet");
+				result [0] = Convert.ToInt32 (wsEntry.Rows);
+				result [1] = Convert.ToInt32 (wsEntry.Cols);
+				return result;
+			} catch (Exception e) {
+				Console.WriteLine (e);
+				return result;
+			}
+		}
+	}
+}
